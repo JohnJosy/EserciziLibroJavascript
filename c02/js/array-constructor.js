fcd74673a551b50c0388a0420c74b2fe34b37b2d
JohnJosy <johnwebfree@gmail.com>
2018-12-28 12:48:54 +0100

    add js costructur-array

---
+++ c02/js/array-constructor.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var colors = new Array( 'white',
+                        'black',
+                        'custom');
+
+var elementColors = document.getElementById('colors')
+elementColors.textContent = colors[1]
