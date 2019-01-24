7c168dd1fc8a5b4eb57b4db2ff30e514d49a68d8
JohnJosy <johnwewbfree@gmail.com>
2019-01-24 16:37:30 +0100

    Inserito ; file piuValoriFunzioni.js

--- c03/js/piuValoriFunzione.js
+++ c03/js/piuValoriFunzione.js
@@ -1,22 +1,22 @@
 'use strict'
-var elementArea = document.getElementById('messageArea')
-var elementVolume = document.getElementById('messageVolume')
+var elementArea = document.getElementById('messageArea');
+var elementVolume = document.getElementById('messageVolume');
 
 function getSize (width, height, depth) {
     var area = width * height;
     var volume = width * height * depth;
     var sizes = [area, volume];
 
-    return sizes
+    return sizes;
 }
 
-var widthPerson = prompt('Inserire Larghezza')
-var heightPerson = prompt('Inserie Altezza')
-var depthPerson = prompt('Inserire Profondità')
+var widthPerson = prompt('Inserire Larghezza');
+var heightPerson = prompt('Inserie Altezza');
+var depthPerson = prompt('Inserire Profondità');
 
 var areaOne = getSize(widthPerson, heightPerson, depthPerson)[0];
 var volumeOne = getSize(widthPerson, heightPerson, depthPerson)[1];
 
-elementArea.textContent = ('Risultato Area ' + areaOne)
-elementVolume.textContent = ('Il valore del volume \xE9: ' + volumeOne)
+elementArea.textContent = ('Risultato Area ' + areaOne);
+elementVolume.textContent = ('Il valore del volume \xE9: ' + volumeOne);
 
