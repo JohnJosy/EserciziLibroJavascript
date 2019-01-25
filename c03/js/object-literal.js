9cd4c9cc0978b21ce110d30695ed6b7b2de8b05c
JohnJosy <johnwewbfree@gmail.com>
2019-01-25 12:16:53 +0100

    Inseriti oggetto litera html e js (creato oggetto con funzione e stampo tutto nell html)

---
+++ c03/js/object-literal.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var elhotelName = document.getElementById('hotelName');
+var elroomsFree = document.getElementById('rooms');
+
+var hotel = {
+   name: 'Quay',
+   rooms: 40,
+   booked: 25,
+   checkAvailability: function () {
+      return this.rooms - this.booked 
+   }
+};
+
+//altro modo di istanziare un oggetto
+/* var hotelName = hotel['name']
+var roomsFree = hotel['checkAvailability'](); */
+
+var hotelName = hotel.name;
+var roomsFree = hotel.checkAvailability();
+
+elhotelName.textContent = hotelName;
+elroomsFree.textContent = roomsFree;
+
+
+
