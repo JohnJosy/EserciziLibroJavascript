e04f58ed54618a4883b6ef1079385fb8b7312f8d
JohnJosy <johnwewbfree@gmail.com>
2019-02-08 11:37:22 +0100

    Creata logica per Object constructor

---
+++ c03/js/object-constructor.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var hotel = new Object();
+hotel.name = 'Park';
+hotel.rooms = 120;
+hotel.booked = 77;
+hotel.checkAvailability = function () {
+    return this.rooms - this.booked
+}
+
+/* Per mofidicare un elemento basta sovrascriverlo
+    shotel.rooms = 100
+   Per eliminare una proprietà mettima delete d'avanti
+    delete hotel.name;
+*/
+
+
+var elName = document.getElementById('hotelName')
+elName.textContent = hotel.name;
+
+
+
+var elFreeRoms = document.getElementById('rooms')
+elFreeRoms.textContent = hotel.checkAvailability()
