c105833c96e4b8e0f30351e24e7f33ed2bfd6400
JohnJosy <johnwewbfree@gmail.com>
2019-02-08 12:58:36 +0100

    Aggiunta logica adding & removing proprietà

---
+++ c03/js/adding-and-removing-properties.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var hotel = {
+    name: 'Park',
+    rooms: 120,
+    booked: 77
+}
+
+hotel.pool = false;
+hotel.gym = true;
+
+var elPool = document.getElementById('pool');
+elPool.className = hotel.pool;
+
+var elGym = document.getElementById('gym');
+elGym.className = hotel.gym;
+
+
