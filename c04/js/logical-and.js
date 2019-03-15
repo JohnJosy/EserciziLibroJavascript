9a39818f865d4f449057d09bab1db7e3f7391ec1
JohnJosy <johnwewbfree@gmail.com>
2019-03-15 15:32:12 +0100

    modificatoLogicalAnds.js(metodo innerHTMLetagStrong)

--- c04/js/logical-and.js
+++ c04/js/logical-and.js
@@ -8,6 +8,6 @@ var pass2 = 6 //Round1 - Punteggio minimo
 //Se l'utente passa in entrambi i Round salva il riisultato in una variabile
 var score = (score1 >= pass1) && (score2 >= pass2)
 
-var msg = 'Both rounds passed: ' + score
+var msg = 'Both rounds passed: ' + '<strong>' + score + '</strong>'
 var elAnswer = document.getElementById('answer')
-elAnswer.textContent = msg
+elAnswer.innerHTML = msg
