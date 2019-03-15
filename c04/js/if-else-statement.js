040c66f45f6266af229603d1673fdcadbc3142c2
JohnJosy <johnwewbfree@gmail.com>
2019-03-15 17:28:47 +0100

    Add if-else-statement.js

--- c04/js/if-else-statement.js
+++ c04/js/if-else-statement.js
@@ -1,13 +1,13 @@
 'use strict'
 var score = 5
 var pass = 50
-var msg
+var msg1
 
 if (score >= pass) {
-    msg = 'Congratulations, you have passed!'
+    msg1 = 'Congratulations, you have passed!'
 } else {
-    msg = 'Have another chance!'
+    msg1 = 'Have another chance!'
 }
 
 var elAnswer = document.getElementById('answer')
-elAnswer.innerHTML = msg
+elAnswer.innerHTML = msg1
